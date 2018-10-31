a42cd99f77e13bd4ae62bf56d663c413bf28b02c
Julia Savych <julliasavych@gmail.com>
2018-10-31 12:49:08 +0200

    doubleNumbers done

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,4 +1,7 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
-  // your logic here...
+  const strNum = num + '';
+
+  const reverseNum = strNum.split('').reverse().join('');
+  return reverseNum.length > 1 && reverseNum === strNum ? num : num * 2;
 };
